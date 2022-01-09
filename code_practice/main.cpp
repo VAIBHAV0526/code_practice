a6a8e0784b7f7fe26956e5e253c615888254f432
VAIBHAV0526 <74946719+VAIBHAV0526@users.noreply.github.com>
2022-01-09 12:11:16 +0530

    Initial Commit

---
+++ code_practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  code_practice
+//
+//  Created by vaibhav singh on 09/01/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
